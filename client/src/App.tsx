eeb043dcb7fe1d3f1f39fd5d48c37466133ac999
ushu3323 <redspower10@gmail.com>
2023-07-02 14:19:30 -0300

    Use airbnb code styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+// eslint-disable-next-line import/no-absolute-path
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,19 +11,25 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={ viteLogo } className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={ reactLogo } className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={ () => setCount((count) => count + 1) }>
-          count is { count }
+        <button type="button" onClick={() => setCount((value) => value + 1)}>
+          count is
+          {' '}
+          { count }
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
