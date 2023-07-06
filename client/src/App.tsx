a31bfe1eb0547d485b11461fc3b820e05327d44c
ushu3323 <redspower10@gmail.com>
2023-07-05 23:35:43 -0300

    Setup tailwindcss and primereact

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,19 @@
-import './App.css';
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
+import { Button } from 'primereact/button';
 import { useState } from 'react';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <Button
+          onClick={() => setCount((value) => value + 1)}
+          label="Count is"
+          badge={count.toString()}
+          outlined
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((value) => value + 1)}>
-          count is
-          {' '}
-          { count }
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
-
-export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { PrimeReactProvider } from 'primereact/api';
+
+import 'primereact/resources/themes/md-dark-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
 import './index.css';
+
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
 );
