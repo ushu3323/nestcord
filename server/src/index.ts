c889bc62af2c172873ef7538ead5d316e74ce11a
ushu3323 <redspower10@gmail.com>
2023-07-14 13:20:57 -0300

    fix logging tRPC name

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,4 +17,4 @@ const server = createHTTPServer({
 const port = Number.parseInt(process.env.PORT || '3000', 10);
 
 // eslint-disable-next-line no-console
-server.server.listen(port, () => { console.log('[TRPC] HTTP server listening at port:', port); });
+server.server.listen(port, () => { console.log('[tRPC] HTTP server listening at port:', port); });
