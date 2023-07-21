826908c80882c8d31e6b725eb9d89c6db313fd5f
ushu3323 <redspower10@gmail.com>
2023-07-21 14:07:08 -0300

    Reduce hash salt rounds
    Reducing rounds improves user registration time

--- server/src/utils/hash.ts
+++ server/src/utils/hash.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 
 export const encryptPassword = async (password: string) => {
-  const salt = await bcrypt.genSalt(20);
+  const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
 };
 
