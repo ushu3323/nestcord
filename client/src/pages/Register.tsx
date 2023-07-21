763f5d7540b4341458cf3236f156f883ae5bfeff
ushu3323 <redspower10@gmail.com>
2023-07-21 15:24:57 -0300

    Submit and validate "Register" page form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,24 +2,72 @@
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
+import { useRef } from 'react';
+import { Toast } from 'primereact/toast';
+import { useFormik } from 'formik';
+import { classNames } from 'primereact/utils';
+import { trpc, Utils } from '../utils/trpc';
 
 export default function Register() {
+  const registerMutation = trpc.users.register.useMutation();
+  type MutationInput = Exclude<typeof registerMutation.variables, undefined>;
+
+  const toast = useRef<Toast>(null);
+  const formik = useFormik({
+    initialValues: { username: '', email: '', password: '' },
+    validateOnChange: false,
+    onSubmit: (values) => registerMutation.mutate(values, {
+      onSuccess: () => toast.current?.show({ severity: 'success', summary: 'Success', detail: 'Registered succesfully!' }),
+      onError(opts) {
+        const zodError: Utils.InferFormattedError<MutationInput> | null = (
+          opts.data?.zodError ?? null
+        );
+
+        if (zodError) {
+          /* eslint-disable no-underscore-dangle */
+          const { username, email, password } = zodError;
+          formik.setErrors({
+            username: username?._errors.join('\n') ?? '',
+            email: email?._errors.join('\n') ?? '',
+            password: password?._errors.join('\n') ?? '',
+          });
+          /* eslint-enable no-underscore-dangle */
+          return;
+        }
+        toast.current?.show({ severity: 'error', summary: 'Error', detail: opts.message });
+      },
+    }),
+  });
+
+  type FieldNames = keyof typeof formik.initialValues;
+
+  const isFormFieldInvalid = (name: FieldNames) => !!(formik.errors[name]);
+
+  const getFormErrorMessage = (name: FieldNames) => (
+    formik.errors[name]
+      ? <small className="p-error">{formik.errors[name]}</small>
+      : <small className="p-error">&nbsp;</small>
+  );
+
   return (
     <div className="h-screen flex justify-center items-center">
+      <Toast ref={toast} />
       <Card className="w-96">
-        <h1 className="text-3xl font-bold text-center">User register</h1>
-        <form className="w-full px-5">
-          <div className="flex flex-col gap-8">
-            <div />
+        <h1 className="text-3xl font-bold text-center mb-10">User register</h1>
+        <form onSubmit={formik.handleSubmit} className="w-full px-5">
+          <div className="flex flex-col gap-2">
             <div>
               <span className="p-float-label">
                 <InputText
                   id="username"
                   name="username"
-                  className="p-inputtext-sm w-full"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  className={classNames('w-full', { 'p-invalid': isFormFieldInvalid('username') })}
                 />
                 <label htmlFor="username">Username</label>
               </span>
+              {getFormErrorMessage('username')}
             </div>
             <div>
               <span className="p-float-label">
@@ -27,10 +75,13 @@ export default function Register() {
                   id="email"
                   name="email"
                   type="email"
-                  className="p-inputtext-sm w-full"
+                  className={classNames('w-full', { 'p-invalid': isFormFieldInvalid('email') })}
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
                 />
                 <label htmlFor="email">E-mail</label>
               </span>
+              {getFormErrorMessage('email')}
             </div>
             <div>
               <span className="p-float-label">
@@ -38,12 +89,15 @@ export default function Register() {
                   id="password"
                   name="password"
                   type="password"
-                  className="p-inputtext-sm w-full"
+                  className={classNames('w-full', { 'p-invalid': isFormFieldInvalid('username') })}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
                 />
                 <label htmlFor="password">Password</label>
               </span>
+              {getFormErrorMessage('password')}
             </div>
-            <Button type="submit" label="Register" className="w-full mt-5" />
+            <Button type="submit" label="Register" className="w-full mt-5" loading={registerMutation.isLoading} />
           </div>
         </form>
       </Card>
