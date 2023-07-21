9d5fda48b10af601498be0f758e7b2107422c434
ushu3323 <redspower10@gmail.com>
2023-07-21 14:17:48 -0300

    Import reactprime styles at the end

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PrimeReactProvider } from 'primereact/api';
 
+import './index.css';
 import 'primereact/resources/themes/md-dark-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
-import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
