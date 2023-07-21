430c4eebb871edee75139999225e0942c97bb8ac
ushu3323 <redspower10@gmail.com>
2023-07-21 14:09:22 -0300

    format zod parse errors

--- server/src/trpc.ts
+++ server/src/trpc.ts
@@ -1,10 +1,23 @@
 import { initTRPC } from '@trpc/server';
+import z from 'zod';
 import { createContext } from './context';
 
 const t = initTRPC
   .context<typeof createContext>()
   .create({
-    errorFormatter: ({ shape }) => shape,
+    errorFormatter(opts) {
+      const { shape, error } = opts;
+      return {
+        ...shape,
+        data: {
+          ...shape.data,
+          zodError:
+            error.code === 'BAD_REQUEST' && error.cause instanceof z.ZodError
+              ? error.cause.format()
+              : null,
+        },
+      };
+    },
   });
 
 export const { router } = t;
